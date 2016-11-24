a914b57e9182662271d6453c2d38bfeb45ff25b7
PC <PC@DESKTOP-9HI5JHG.lan>
2016-11-24 17:08:42 +0100

    Sudoku TDD

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,8 +2,85 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
-	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+	public static final int ASCII_ZERO = 48;
+
+	public static boolean[] verify(String candidateSolution) throws Exception {
+
+		// Prerequisiti: la stringa di input deve avere lunghezza 81
+		if (candidateSolution.length() != 81)
+			throw new Exception();
+
+		/*
+		 * error[0] = true se viola la regola 1, false altrimenti error[1] =
+		 * true se viola la regola 2, false altrimenti error[2] = true se viola
+		 * la regola 3, false altrimenti error[3] = true se viola la regola 4,
+		 * false altrimenti
+		 */
+		boolean[] error = { false, false, false, false };
+
+		// CONTROLLO REGOLA 1
+		for (int i = 0; i < candidateSolution.length(); i++)
+			if (candidateSolution.charAt(i) - ASCII_ZERO < 1 || candidateSolution.charAt(i) - ASCII_ZERO > 9)
+				error[0] = true;
+
+		// CONTROLLO REGOLA 2
+		String subgrid = "";
+
+		for (int j = 0; j < 9; j++) {
+
+			for (int i = candidateSolution.length() / 3 * j; i < candidateSolution.length() / 3 * (j + 1); i++)
+				if (i % 9 >= j * 3 && i % 9 <= j * 3 + 2)
+					subgrid += candidateSolution.charAt(i);
+
+			if (repeated(subgrid))
+				error[1] = true;
+			
+			subgrid = "";
+
+		}
+		
+		// CONTROLLO REGOLA 3
+		subgrid = "";
+		
+		for (int i = 0; i < 81; i += 9) {
+			subgrid += candidateSolution.substring(i, i + 9);
+			if (repeated(subgrid))
+				error[2] = true;
+		}
+		
+		// CONTROLLO REGOLA 4
+		subgrid = "";
+		
+		for (int i = 0; i < 9; i++) {
+			
+			for (int j = 0; j < 9; j++)
+				subgrid += candidateSolution.charAt(i + j*9);
+			
+			if (repeated(subgrid))
+				error[3] = true;
+			
+			subgrid = "";
+		}
+		
+
+		return error;
+	}
+
+	/**
+	 * @param true
+	 *            se si ripetono
+	 * @return
+	 */
+	private static boolean repeated(String x) {
+		boolean repeated = false;
+
+		for (int i = 0; i < x.length() - 1; i++) {
+			for (int j = i + 1; j < x.length(); j++) {
+				if (x.charAt(i) == x.charAt(j))
+					repeated = true;
+			}
+		}
+
+		return repeated;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -1,14 +1,47 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void positiveDigitsFrom1To9Only() throws Exception {
+
+		Assert.assertEquals(true, SudokuVerifier
+				.verify("011111111111111111111111111111111111111111111111111111111111111111111111111111111")[0]);
+		
+		Assert.assertEquals(true, SudokuVerifier
+				.verify("3581059fs3581059fs3581059fs3581059fs3581059fs3581059fs3581059fs3581059fs3581059fs")[0]);
+	}
+
+	@Test
+	public void noRepeatedDigitsInEverySubgrid() throws Exception {
+
+		Assert.assertEquals(true, SudokuVerifier
+				.verify("111234567111345678111567893738591473738591473738591473738591473738591473753859147")[1]);
+		
+		Assert.assertEquals(true, SudokuVerifier
+				.verify("111234567111345678111567893738591473738591473738591473738591473738591473753859147")[1]);
+		
+		Assert.assertEquals(false, SudokuVerifier
+				.verify("417369825632158947958724316825437169791586432346912758289643571573291684164875293")[1]);
+	}
+	
+	@Test
+	public void noRepeatedDigitsInEveryRow() throws Exception {
+		
+		Assert.assertEquals(true, SudokuVerifier
+				.verify("123456789123456789123456789123456789123456789123456789123456789123456789123456788")[2]);
+		
+	}
+	
+	@Test
+	public void noRepeatedDigitsInEveryColumn() throws Exception {
+		
+		Assert.assertEquals(true,  SudokuVerifier
+				.verify("417369825632158947958724316825437169791586432346912758289643571573291683164875293")[3]);
 	}
 
 }
